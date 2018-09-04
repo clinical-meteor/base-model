7cc71b5845f78893f33fc10154dda5b793b622ec
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-09-03 21:03:06 -0500

    .circle/config.yml

---
+++ tests/BaseModelTests.tests.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+import { chai } from 'meteor/practicalmeteor:chai';
+import { expect } from 'meteor/practicalmeteor:chai';
+
+describe('clinical:basemodel', function () {
+  beforeEach(function () {
+    //console.log('beforeEach');
+  });
+  afterEach(function () {
+    //console.log('afterEach');
+  });
+  it('BaseModel object exists globally', function () {
+    expect(BaseModel).to.exist;
+  });
+});
